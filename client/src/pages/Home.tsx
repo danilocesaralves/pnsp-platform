29156c8a7792e95d703bc43f786888f0b8b52742
Danilo Cesar <composisamba@gmail.com>
2026-05-13 16:46:42 -0300

    fix: foto position + subtitulo identico mockup

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -642,7 +642,7 @@ export default function Home() {
           <img
             src="/hero-musicians.jpg"
             alt="Músicos de samba"
-            style={{ width: "100%", height: "100%", objectFit: "cover", objectPosition: "center center" }}
+            style={{ width: "100%", height: "100%", objectFit: "cover", objectPosition: "55% center" }}
           />
           {/* Gradiente da esquerda (preto) para transparente */}
           <div style={{
