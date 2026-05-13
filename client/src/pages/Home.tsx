2c49af91831e69056ec113656aff97ed2feab400
Danilo Cesar <composisamba@gmail.com>
2026-05-13 17:50:09 -0300

    fix: foto hero right center

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -643,7 +643,7 @@ export default function Home() {
           <img
             src="/hero-musicians.jpg"
             alt="Músicos de samba"
-            style={{ width: "100%", height: "100%", objectFit: "cover", objectPosition: "center center" }}
+            style={{ width: "100%", height: "100%", objectFit: "cover", objectPosition: "right center" }}
           />
           {/* Gradiente da esquerda (preto) para transparente */}
           <div style={{
